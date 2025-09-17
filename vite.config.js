0009e235ef784a12b479eb653e451eebcbabde57
Shimaa49 <shimaamounir501@gmail.com>
2025-09-17 04:13:38 +0100

    Fix base + add 404 for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/UserDashboard/"
-
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        copyFileSync(
+          resolve(__dirname, 'dist/index.html'),
+          resolve(__dirname, 'dist/404.html')
+        )
+      }
+    }
+  ],
+  base: "/UserDashboard/",
 })
